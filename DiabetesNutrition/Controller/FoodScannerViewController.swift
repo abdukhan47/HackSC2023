9ffab12745e607855a5dc7399622d1ea0455e066
Abdulah Khan <amkhan@usc.edu>
2023-02-04 16:59:43 -0800

    Fixed Scanner

--- DiabetesNutrition/Controller/FoodScannerViewController.swift
+++ DiabetesNutrition/Controller/FoodScannerViewController.swift
@@ -11,26 +11,38 @@ final class FoodScannerViewController: UIViewController {
                                                                       isGuidanceEnabled: true,
                                                                       isHighlightingEnabled: true) // Mark 1
     
+    @IBOutlet weak var openCamera: UIButton!
+    
     private var isScannerAvailable: Bool { DataScannerViewController.isSupported && DataScannerViewController.isAvailable } // Mark 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dataScannerViewController.delegate = self // Mark 5
         
+//        if isScannerAvailable { // Mark 2
+//            present(dataScannerViewController, animated: true) // Mark 4
+//            try? dataScannerViewController.startScanning() // Mark 4
+//        }
+    }
+    
+    @IBAction func openCameraButton(_ sender: Any) {
         if isScannerAvailable { // Mark 2
             present(dataScannerViewController, animated: true) // Mark 4
             try? dataScannerViewController.startScanning() // Mark 4
         }
     }
+    
 }
 
 extension FoodScannerViewController: DataScannerViewControllerDelegate { // Mark 5
     func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) { // Mark 5
         for item in addedItems {
             switch item {
-
+                
                 //process(data: text.transcript)
             case .barcode(let barcode):
+                let upc = barcode.payloadStringValue!
+                process(data: upc)
                 print(barcode.payloadStringValue!)
                 
                 break
@@ -43,15 +55,33 @@ extension FoodScannerViewController: DataScannerViewControllerDelegate { // Mark
     }
     
     private func process(data: String) { // Mark 6
-        guard let mathObject = MathObject(inputData: data) else {
-            print("Could not parse into MathObject")
-            return
+        let appId = "5f213012"
+        let appKey = "34c873ef96c3708a11b160d10228b1c4"
+        let upc = data
+        
+        let urlString = "https://api.nutritionix.com/v1_1/item?upc=\(upc)&appId=\(appId)&appKey=\(appKey)"
+        
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let error = error {
+                    print("Error: \(error)")
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    if response.statusCode == 200 {
+                        // success, parse the response data here
+                        do {
+
+                            let nutritionInfo = try JSONDecoder().decode(Food.self, from: data)
+                                print(nutritionInfo)
+                            print(nutritionInfo)
+                        } catch {
+                            print("Error: \(error)")
+                        }
+                    } else {
+                        print("Failure: \(response.statusCode)")
+                    }
+                }
+            }
+            task.resume()
         }
-        
-        dismiss(animated: true)
-        
-        let alertViewController = UIAlertController(title: "Math Solver", message: "The result of your calculus is: \(mathObject.result)", preferredStyle: .alert)
-        alertViewController.addAction(UIAlertAction(title: "Holy Swift!", style: .cancel))
-        present(alertViewController, animated: true)
     }
 }
--- DiabetesNutrition/Controller/NutritionViewController.swift
+++ DiabetesNutrition/Controller/NutritionViewController.swift
@@ -1,51 +1,51 @@
+////
+////  NutritionViewController.swift
+////  DiabetesNutrition
+////
+////  Created by Abdulah Khan on 2/3/23.
+////
 //
-//  NutritionViewController.swift
-//  DiabetesNutrition
+//import UIKit
+//import Charts
 //
-//  Created by Abdulah Khan on 2/3/23.
 //
-
-import UIKit
-import Charts
-
-
-class NutritionViewController: UIViewController {
-    
-    
-    @IBOutlet weak var lblFoodName : UILabel!
-    @IBOutlet weak var lblCalNum : UILabel!
-    
-    func showFoodDetails(_ food: Food)
-    {
-        lblFoodName.text = food.name;
-        lblCalNum.text = String(format: "%.1f", food.cal);
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let targetFood = Food(name: "Potato",
-                              cal: 30.0,
-                              nDict: ["Protein": 1.0, "Fat":2.0])
-        
-        self.showFoodDetails(targetFood)
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
-
-
-
-
+//class NutritionViewController: UIViewController {
+//
+//
+//    @IBOutlet weak var lblFoodName : UILabel!
+//    @IBOutlet weak var lblCalNum : UILabel!
+//
+//    func showFoodDetails(_ food: Food)
+//    {
+//        lblFoodName.text = food.name;
+//        lblCalNum.text = String(format: "%.1f", food.cal);
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        let targetFood = Food(name: "Potato",
+//                              cal: 30.0,
+//                              nDict: ["Protein": 1.0, "Fat":2.0])
+//
+//        self.showFoodDetails(targetFood)
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
+//
+//
+//
+//
