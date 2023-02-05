c0dc0b0a9eed4d55a8eaec00064c6f23ec9666b3
Abdulah Khan <amkhan@usc.edu>
2023-02-04 16:21:56 -0800

    Scanner working.

--- DiabetesNutrition/Controller/FoodScannerViewController.swift
+++ DiabetesNutrition/Controller/FoodScannerViewController.swift
@@ -1,29 +1,57 @@
-//
-//  FoodScannerViewController.swift
-//  DiabetesNutrition
-//
-//  Created by Abdulah Khan on 2/3/23.
-//
-
 import UIKit
+import VisionKit
 
-class FoodScannerViewController: UIViewController {
-
+final class FoodScannerViewController: UIViewController {
+    
+    private let dataScannerViewController = DataScannerViewController(recognizedDataTypes: [.barcode()],
+                                                                      qualityLevel: .fast,
+                                                                      recognizesMultipleItems: false,
+                                                                      isHighFrameRateTrackingEnabled: true,
+                                                                      isPinchToZoomEnabled: true,
+                                                                      isGuidanceEnabled: true,
+                                                                      isHighlightingEnabled: true) // Mark 1
+    
+    private var isScannerAvailable: Bool { DataScannerViewController.isSupported && DataScannerViewController.isAvailable } // Mark 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataScannerViewController.delegate = self // Mark 5
+        
+        if isScannerAvailable { // Mark 2
+            present(dataScannerViewController, animated: true) // Mark 4
+            try? dataScannerViewController.startScanning() // Mark 4
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension FoodScannerViewController: DataScannerViewControllerDelegate { // Mark 5
+    func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) { // Mark 5
+        for item in addedItems {
+            switch item {
+
+                //process(data: text.transcript)
+            case .barcode(let barcode):
+                print(barcode.payloadStringValue!)
+                
+                break
+            case .text(_):
+                break
+            @unknown default:
+                print("Should not happen")
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func process(data: String) { // Mark 6
+        guard let mathObject = MathObject(inputData: data) else {
+            print("Could not parse into MathObject")
+            return
+        }
+        
+        dismiss(animated: true)
+        
+        let alertViewController = UIAlertController(title: "Math Solver", message: "The result of your calculus is: \(mathObject.result)", preferredStyle: .alert)
+        alertViewController.addAction(UIAlertAction(title: "Holy Swift!", style: .cancel))
+        present(alertViewController, animated: true)
     }
-    */
-
 }
